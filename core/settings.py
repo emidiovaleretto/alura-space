8287cc5c919c438257791f5d32303ecb39e3ff63
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-11 19:45:01 +0000

    chore: hide secret key in settings.py

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m8a=_!jn4oxdzpo3n40r85d62n%gd(l7=4@b)zo%i_c3ll=p8y'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '8000-emidiovalere-aluraspace-uofnhnbclod.ws-us116.gitpod.io'
+]
 
 
 # Application definition
