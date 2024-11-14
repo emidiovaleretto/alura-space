592a8d5f1963ad3ba49f0c692693045c7db9799b
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-14 00:20:37 +0000

    chore: retrive all photos from db and display them in the index page

--- galleries/urls.py
+++ galleries/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('imagem/', views.imagem, name='imagem'),
+    path('imagem/<int:id>/', views.imagem, name='imagem'),
 ]
--- galleries/views.py
+++ galleries/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Photo
 
 
 def index(request):
-    return render(request, 'galleries/index.html')
+    photos = Photo.objects.all()
+    return render(request, 'galleries/index.html', {'photos': photos})
 
 
-def imagem(request):
-    return render(request, 'galleries/imagem.html')
+def imagem(request, id):
+    photo = get_object_or_404(Photo, pk=id)
+    return render(request, 'galleries/imagem.html', {'photo': photo})
