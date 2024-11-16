38962dccaed256e0d18244f48c0d27349a4f294c
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-16 17:50:17 +0000

    chore: add login_required to views to protect the routes

--- galleries/views.py
+++ galleries/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Photo
 
 
+@login_required(login_url='/auth/signin')
 def index(request):
     photos = Photo.objects.filter(published=True)
     categories = Photo.objects.values_list('category', flat=True).distinct()
     return render(request, 'galleries/index.html', {'photos': photos, 'categories': categories})
 
 
+@login_required(login_url='/auth/signin')
 def imagem(request, id):
     photo = get_object_or_404(Photo, pk=id)
     return render(request, 'galleries/imagem.html', {'photo': photo})
 
 
+@login_required(login_url='/auth/signin')
 def search(request):
     search = request.GET.get('search')
     photos = Photo.objects.filter(name__icontains=search)
