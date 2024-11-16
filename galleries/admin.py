05c66820018410878c6b353ea3403b594a50a6e7
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-16 10:33:37 +0000

    chore: customise Photo admin

--- galleries/admin.py
+++ galleries/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Photo
 
-# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'caption', 'created_at', 'published')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_filter = ('category',)
+    list_editable = ('published',)
+    list_per_page = 10
+
+
+admin.site.register(Photo, PhotoAdmin)
