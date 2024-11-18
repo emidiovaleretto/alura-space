4f4190956231c67eb1471a8cc2a9283f1d61da48
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-18 12:30:22 +0000

    chore: implement a view to add an image

--- galleries/views.py
+++ galleries/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from django.contrib.messages import constants as message_tag
 from .models import Photo
 from .forms import PhotoForm
 
@@ -13,6 +16,43 @@ def index(request):
 
 def add_image(request):
     form = PhotoForm()
+
+    if request.method == "POST":
+        form = PhotoForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            caption = form.cleaned_data.get('caption')
+            description = form.cleaned_data.get('description')
+            category = form.cleaned_data.get('category')
+            image = form.cleaned_data.get('image')
+
+            form = Photo.objects.create(
+                name=name,
+                caption=caption,
+                description=description,
+                category=category,
+                image=image,
+                user=request.user
+            )
+
+            form.save()
+            messages.add_message(
+                request,
+                message_tag.SUCCESS,
+                'Imagem adicionada com sucesso!'
+            )
+            return redirect('index')
+
+        messages.add_message(
+            request,
+            message_tag.ERROR,
+            'Não foi possível adicionar a imagem. Verifique os campos obrigatórios.'
+        )
+
+    else:
+        form = PhotoForm()
+
     return render(request, 'galleries/add_image.html', {'form': form})
 
 
