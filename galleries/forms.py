b369b80e25d54e7f4c71808d573c767bf964011b
Emidio Valeretto <emidio.valereto@gmail.com>
2024-11-18 17:56:40 +0000

    refactor: update PhotoForm to simplify and improve functionality

--- galleries/forms.py
+++ galleries/forms.py
@@ -1,58 +1,47 @@
 from django import forms
+from .models import Photo
 
 
-class PhotoForm(forms.Form):
-    name = forms.CharField(
-        label='Nome',
-        max_length=100,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'input-text',
-                'placeholder': 'Ex.: Aurora Boreal'
-            }
-        )
-    )
-    caption = forms.CharField(
-        label='Legenda',
-        max_length=100,
-        widget=forms.TextInput(
-            attrs={
-                'class': 'input-text',
-                'placeholder': 'Ex.: Aurora Boreal em Tromso, Noruega'
-            }
-        )
-    )
-    description = forms.CharField(
-        label='Descrição',
-        widget=forms.Textarea(
-            attrs={
-                'class': 'input-text',
-                'placeholder':
-                'Ex.: A aurora boreal é um fenômeno natural que ocorre em regiões polares.'
-            }
-        )
-    )
-    category = forms.ChoiceField(
-        label='Categoria',
-        choices=(
-            ('NEBULOSA', 'Nebulosa'),
-            ('GALAXIA', 'Galaxia'),
-            ('PLANETA', 'Planeta'),
-            ('ESTRELLA', 'Estrela'),
-        ),
-        widget=forms.Select(
-            attrs={
-                'class': 'input-text',
-                'placeholder': 'Select a category'
-            }
-        )
-    )
-    image = forms.ImageField(
-        label='Imagem',
-        widget=forms.FileInput(
-            attrs={
-                'class': 'input-text',
-                'placeholder': 'Select an image'
-            }
-        )
-    )
+class PhotoForm(forms.ModelForm):
+    '''
+    Formulário para adicionar uma nova imagem
+    '''
+    name = forms.CharField(max_length=100, required=True)
+    caption = forms.CharField(max_length=100, required=True)
+    description = forms.CharField(widget=forms.Textarea())
+    category = forms.ChoiceField(choices=Photo.CATEGORY_CHOICES, required=True)
+    image = forms.ImageField()
+
+    class Meta:
+        '''
+        Meta informações do formulário
+        '''
+        model = Photo
+        fields = ['name', 'caption', 'description', 'category', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'name': 'Ex.: Aurora Boreal',
+            'caption': 'Ex.: Aurora Boreal em Tromso, Noruega',
+            'description':
+            'Ex.: A aurora boreal é um fenômeno natural que ocorre em regiões polares.',
+            'category': 'Selecione uma categoria',
+            'image': 'Selecione uma imagem'
+        }
+
+        print(self.fields)
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            if field != 'category':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]}*'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            else:
+                self.fields[field].label = placeholders[field]
+            self.fields[field].widget.attrs['class'] = 'input-text'
